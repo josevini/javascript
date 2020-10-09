8223e5bbf3748bf126d330ae15d761361943bcbc
José Vinícius Silva <ferreiravini468@gmail.com>
2020-10-08 23:52:35 -0300

    update index.html

---
+++ Validar CPF/js/script.js
@@ -0,0 +1,14 @@
+function validar () {
+    let form_cpf = window.document.getElementById('form_cpf')
+    form_cpf.focus()
+    let cpf = form_cpf.value
+    let novo_cpf = 0
+    for (let pos = 0; pos < cpf.length; pos++) {
+        // if (cpf[pos] >= 0 && cpf[pos] <= 9) {
+        //     novo_cpf += cpf[pos]
+        // } else {
+        //     return false
+        // }
+    }
+
+}
