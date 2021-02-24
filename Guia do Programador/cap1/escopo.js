edff1e074ca01db19090ed729e53555c27f30fb1
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-02-24 20:48:45 -0300

    Update escopo.js

---
+++ Guia do Programador/cap1/escopo.js
@@ -0,0 +1,13 @@
+returnMsg1 = function () {
+    msg = 'Hello, World!'
+    console.log(msg)
+}
+
+returnMsg2 = function () {
+    var msg = 'Hello, JavaScript!'
+    console.log(msg)
+}
+
+returnMsg1()
+returnMsg2()
+console.log(msg)
