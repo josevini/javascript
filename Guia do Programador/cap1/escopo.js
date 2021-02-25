57f84518b0dc1c137a8f68f3652093209b63d944
José Vinícius Silva Ferreira <ferreiravini468@gmail.com>
2021-02-24 21:06:32 -0300

    Update escopo.js

--- Guia do Programador/cap1/escopo.js
+++ Guia do Programador/cap1/escopo.js
@@ -1,3 +1,10 @@
+/*
+    Conceitos relativos ao EcmaScript 5
+
+    O uso palavra-chave var cria uma variável de escopo local
+    A omissão da palavra-chave var cria uma variável de escopo global
+*/
+
 returnMsg1 = function () {
     msg = 'Hello, World!'
     console.log(msg)
