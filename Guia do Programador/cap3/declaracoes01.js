efc49a3abfe9acb27d728227972ac2f30ec8ccaf
José Vinícius Silva <ferreiravini468@gmail.com>
2021-03-04 00:13:13 -0300

    Update declaracoes01.js

---
+++ Guia do Programador/cap3/declaracoes01.js
@@ -0,0 +1,9 @@
+/*
+    var - declara uma variável
+    function - declara uma função
+    return - retorna um valor
+*/
+
+var nome = 'Vinicius', sobrenome = 'Silva', idade = 18
+// console.log(`${nome} ${sobrenome} tem ${idade} anos.`)
+
