ec17bf13c7847e60309bb3d6615eee6f1d9300b1
José Vinícius Silva <ferreiravini468@gmail.com>
2021-03-04 00:31:25 -0300

    Update declaracoes01.js

--- Guia do Programador/cap3/declaracoes01.js
+++ Guia do Programador/cap3/declaracoes01.js
@@ -4,24 +4,17 @@
     return - retorna um valor
 */
 
-// Declarando 3 variáveis
 var nome = 'Vinicius', sobrenome = 'Silva', idade = 18
 
-// Declarando uma função showInfo com 3 parâmetros
 function showInfo (n, s, i) {
-    // Exibe valores passados como argumentos
     console.log(`${n} ${s} tem ${i} anos.`)
 }
 
 showInfo(nome, sobrenome, idade)
 
 function retornaSoma (n1, n2) {
-    /*
-        Guarda a soma entre
-        dois argumentos
-    */
     var soma = n1 + n2
-    return soma // retorna a soma
+    return soma
 }
 
 var resultado = retornaSoma(3, 4)
