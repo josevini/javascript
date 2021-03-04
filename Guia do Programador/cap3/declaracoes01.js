2c376b5396cd1addb2f5edebb205d4b780e96665
José Vinícius Silva <ferreiravini468@gmail.com>
2021-03-04 00:16:57 -0300

    Update declaracoes01.js

--- Guia do Programador/cap3/declaracoes01.js
+++ Guia do Programador/cap3/declaracoes01.js
@@ -4,6 +4,13 @@
     return - retorna um valor
 */
 
+// Declarando 3 variáveis
 var nome = 'Vinicius', sobrenome = 'Silva', idade = 18
-// console.log(`${nome} ${sobrenome} tem ${idade} anos.`)
 
+// Declarando uma função showInfo com 3 parâmetros
+function showInfo (n, s, i) {
+    // Exibe valores passados como argumentos
+    console.log(`${n} ${s} tem ${i} anos.`)
+}
+
+showInfo(nome, sobrenome, idade)
