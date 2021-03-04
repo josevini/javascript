4be65fd0eb42f163345e2128f88c110e831b81bb
José Vinícius Silva <ferreiravini468@gmail.com>
2021-03-03 21:41:09 -0300

    Update declaracoes02.js

--- Guia do Programador/cap3/declaracoes02.js
+++ Guia do Programador/cap3/declaracoes02.js
@@ -5,6 +5,8 @@
     else {bloco} - é executado caso o bloco do if seja falso
 
     else if - estrutura aninhada
+
+    switch () {} - testa mútiplos valores
 */
 
 var media = 10
@@ -15,4 +17,20 @@ if (media < 5) {
     console.log('Recuperação')
 } else {
     console.log('Aprovado')
+}
+
+console.log('----------------------')
+
+var op = 0
+
+console.log('1 - login\n2 - logout')
+switch (op) {
+    case 1:
+        console.log('Você fez login')
+        break
+    case 2:
+        console.log('Você fez logout')
+        break
+    default:
+        console.log('ERRO')
 }
