765e03a1ee44a1c8493b31b9e7d4eb1820322cce
José Vinícius Silva <ferreiravini468@gmail.com>
2021-03-05 22:37:30 -0300

    Update objetos01.js

--- Guia do Programador/cap4/objetos01.js
+++ Guia do Programador/cap4/objetos01.js
@@ -8,3 +8,11 @@ pessoa = new Object()
 pessoa.nome = 'Vinicius'
 pessoa.sobrenome = 'Silva'
 pessoa['idade'] = 18
+
+// Sintaxe literal
+caneta = {
+    ponta: 0.5,
+    marca: 'BIC',
+    cor: 'preta',
+    tampada: true
+}
