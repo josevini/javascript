16cf9d60f8b28f0fa4e35a61b8a1d0e981935b15
José Vinícius Silva Ferreira <62161557+josevini@users.noreply.github.com>
2022-05-26 22:05:39 -0300

    Delete Guia Definitivo directory

--- Guia Definitivo/capitulo1/funcoes01.js
+++
@@ -1,9 +0,0 @@
-function plus(x) {
-    return x+1
-} // mostra o sucessor do valor passado por parâmetro 
-
-var square = function(x) {
-    return x*x
-} // multiplica o valor informado por ele mesmo
-
-console.log(square(plus(9))) // a function square() recebe plus() como parâmetro
--- Guia Definitivo/capitulo1/funcoes02.js
+++
@@ -1,7 +0,0 @@
-function abs(x) {
-    if (x >= 0) {
-        return x
-    } else {
-        return -x
-    }
-} // calcula o valor absoluto
